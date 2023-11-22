a8cbbaf12b8d50a75c85a4e37d493271c5b0de34
Victor Araya <112587750+VitokoUcn@users.noreply.github.com>
2023-11-21 23:07:18 -0300

    Update ProfileScreen.jsx

--- src/views/UserTab/ProfileScreen.jsx
+++ src/views/UserTab/ProfileScreen.jsx
@@ -1,12 +1,77 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image, Button, } from 'react-native';
+import { styles, stylesProfile } from '../../theme/profileTheme';
+import { useNavigation } from '@react-navigation/native';
+import { Background } from '../../components/Background';
+
+
+
+
+
 
 const ProfileScreen = () => {
-  return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
-  )
-}
+    const navigation = useNavigation();
+    const goToBottomNavigator = () => {
+        navigation.navigate('BottomNavigator');
+    };
+    return (
+        <>
+        <Background />
+        <View style={stylesProfile.container}>
+            <View style={stylesProfile.profileHeader}>
+                <Text style={stylesProfile.username}>JhonDev</Text>
+            </View>
+            
+            <View style={stylesProfile.profileImageContainer}>
+                <Image
+                    source={{ uri: 'https://cdn.gogeticon.net/files/1925428/fa0cbc2764f70113bf2fad3905933545.png' }}
+                    style={stylesProfile.profileImage}
+                />
+            </View>
+            
+            
+            <View style={stylesProfile.profileStats}>
+                <View style={stylesProfile.profileInfo}>
+                <Text style={stylesProfile.realName}>John Doe</Text>
+                <Text style={stylesProfile.role}>Database Administrator</Text>
+            </View>
+            <View style={stylesProfile.stat}>
+                    <Text style={stylesProfile.statCount}>4</Text>
+                    <Text style={stylesProfile.statLabel}>Posts</Text>
+                </View>
+            </View>
+            
+            <View style={stylesProfile.profileBio}>
+            
+                <View style={stylesProfile.profileStats}>
+                <Text style={stylesProfile.bioHeading}>Soft Skills:</Text>
+                <Text style={stylesProfile.box }>Creatividad</Text>
+                <Text style={stylesProfile.box }>Pensamiento Critico</Text>
+                </View>
+                <Text style={stylesProfile.bioText}>Administrador senior de base de datos
+Alegre y fiel trabajador</Text>
+            </View>
+            <View style={stylesProfile.buttonAlign}>
+                <TouchableOpacity style={stylesProfile.editProfileButton}>
+                    <Text style={stylesProfile.editProfileButtonText}>Editar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={stylesProfile.editProfileButton}>
+                    <Text style={stylesProfile.editProfileButtonText}>Descargar CV</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={stylesProfile.profilePosts}>
+                {/* Post deberian ir aqui */}
+            </View>
+            
+            
+        </View>
 
-export default ProfileScreen
+
+        </>
+    );
+};
+
+
+
+
+export default ProfileScreen;
